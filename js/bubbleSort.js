4ecd9e3dbb637d8243a6b539772151c705c2508a
hojongyu2 <yuhojo91@gmail.com>
2023-02-23 16:40:46 -0600

    bubble sort using JS and Python

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -2,6 +2,24 @@ var sequence = [4, 3, 5, 0, 1]
 var swaps = 0
 
 // Your Code Here
+function result (numberList) {
+    let isSwapped = true
+    while (isSwapped) {
+        isSwapped = false
+        for (let i = 0; i < numberList.length; i++){
+            let prev = numberList[i]
+            let curr = numberList[i+1]
+            if (numberList[i] > numberList[i+1]){
+                numberList[i] = curr
+                numberList[i+1] = prev
+                isSwapped = true
+            }
+        }
+    }
+    return numberList
+}
 
-console.log("Final result: " + result)
+
+
+console.log("Final result: " + result(sequence))
 console.log("Swaps: " + swaps)
--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,19 @@
 sequence = [4, 3, 5, 0, 1]
-swaps = 0
 
 # Your Code Here
+def bubble_sort(list_of_nums):
+    swaps = 0
+    is_swapped = True
+    while is_swapped:
+        is_swapped = False
+        for idx in range(len(list_of_nums)-1):
+            if list_of_nums[idx] > list_of_nums[idx+1]:
+                swaps += 1
+                is_swapped = True
+                list_of_nums[idx], list_of_nums[idx + 1] = list_of_nums[idx+1], list_of_nums[idx]
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+    return list_of_nums, swaps
+
+
+print(f"Final result: {bubble_sort(sequence)}")
+# print(f"Swaps: {swaps}")
